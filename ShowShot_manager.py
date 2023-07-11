10e2906880dc2513c6fc10ff1cb7ceb0ce98ee11
aspyn72 <139076588+aspyn72@users.noreply.github.com>
2023-07-10 19:24:32 -0700

    initatialize filename and jason

--- ShowShot_manager.py
+++ ShowShot_manager.py
@@ -1,12 +1,14 @@
 import os
+import json
 # directory
-os.makedirs()
+#os.makedirs()
 
 class Template:
-    def __init__(self, name=str(""), duration=float(""), status=str("")):
-        self._name = name
-        self._duration=duration
-        self._status=status
+    def __init__(self, name="ShowShot", duration=90.00, status="Done",filename="filename"):
+        self.name = name
+        self.duration=duration
+        self.status=status
+        self.filename=filename
     '''
     def title_or_name(self):
         return self.name
@@ -14,35 +16,100 @@ class Template:
         return self.duration
     def status(self):
         return self.status'''
-    def create_and_add(self,name,duration,status):
+    
+    def create_folder(self,name,duration,status):
         pass
         #new_show=Show(name, duration, status)
         #self.SHOW_DB.append(new_show)
-    def delete(self,name):
+
+    def add_folder(self,filename="filename",name="new_name",duration=80.0,status='no'):
+        with open(filename,'w+') as file:
+            json.dump(file,{"name":name,"duration":duration,"status":status})
+
+    def delete_folder(self,name):
         pass
-    def get_info(self,name):
+
+    def get_single_info(self,name):
         print("It's working")
 
+    # edits
+    def edit_name(self,new_name):
+        self.status=new_name
+
+    def edit_duration(self,new_duration):
+        self.status=new_duration
+    
+    def edit_status(self,new_status):
+        self.status=new_status
+
+
 class Show(Template):
-    def __init__(self):
-        super(Show, self).__init__(name=str)
-        super(Show, self).__init__(duration=)
-        super(Show, self).__init__(status)
+    '''def __init__(self):
+        super(Show, self).__init__(name="Showtime")
+        super(Show, self).__init__(duration=90)
+        super(Show, self).__init__(status="In Progress")
+        self.SHOW_DB=[]'''
+    def __init__(self, name="Showtime", duration=60.00, status="In Progress"):
+        super().__init__(name, duration, status)
         self.SHOW_DB=[]
     
-    def create_and_add(self,name, duration, status):
-        new_show=Show(name,name, duration, status)
+    def create_folder(self,name, duration, status):
+        new_show=Show(name, duration, status)
         self.SHOW_DB.append(new_show)
+    
+    def add_folder(self,filename="filename",name="add_name",duration=70.0,status='si'):
+        with open(filename,'w+') as file:
+            json.dump(file,{"name":name,"duration":duration,"status":status})
 
-     def delete(self,name):
+    def delete_folder(self,name):
         removed_show=Show(name)
         self.SHOW_DB.remove(removed_show)
 
-    def get_info(self,name):
+    def get_single_info(self,name):
         print("It's working")
 
+    # edit
+
+    def edit_name(self, new_name):
+        return super().edit_name(new_name)
+    
+    def edit_duration(self, new_duration):
+        return super().edit_duration(new_duration)
+    
+    def edit_status(self, new_status):
+        return super().edit_status(new_status)
+
 class Shot(Template):
-    pass
+    def __init__(self, name="01_A", duration=5.00, status="Done"):
+        super().__init__(name, duration, status)
+        self.SHOT_DB=[]
+    
+    def create_folder(self,name, duration, status):
+        new_shot=Shot(name, duration, status)
+        self.SHOT_DB.append(new_shot)
+
+    def delete_folder(self,name):
+        removed_shot=Shot(name)
+        self.SHOT_DB.remove(removed_shot)
+
+    def get_single_info(self,name):
+        print("It's working")
+
+    def read_all_shots(self,filename="ShotList.txt"):
+        with open(filename,'r') as file:
+            data=json.load(file)
+        return Shot(**data)
+    
+    # edit
+    
+    def edit_name(self, new_name):
+        return super().edit_name(new_name)
+    
+    def edit_duration(self, new_duration):
+        return super().edit_duration(new_duration)
+    
+    def edit_status(self, new_status):
+        return super().edit_status(new_status)
 
 ######
 show = Show()
