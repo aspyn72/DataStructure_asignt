0ccd8e44327b5b73635688b7307ee48074ef98c8
aspyn72 <139076588+aspyn72@users.noreply.github.com>
2023-07-14 21:09:56 -0700

    unitTesting not working

--- Test_ShowShot_manager_AspynM.py
+++ Test_ShowShot_manager_AspynM.py
@@ -2,79 +2,93 @@ import unittest
 import os
 import shutil
 from json import load
-from ShowShot_manager_AspynM import *
+import json
+from ShowShot_manager_AspynM import Show, Shot
 
-TEST_TEMP_DIR_PATH = "/Users/moon/Library/CloudStorage/OneDrive-BCIT/TERM 3/P2_DataStructure/Real_Test/"
-TEST_TEMP_NAME = "Show_Shot_DB" 
-    # need to put show folder name here to avoid putting the path manually
-TEST_SHOW_DIR_PATH = TEMP_DIR_PATH + TEMP_NAME + "/"
-TEST_SHOW_NAME = "Mimic"
-    # need to put shot folder name here to avoid putting the path manually
-TEST_SHOT_DIR_PATH = SHOW_DIR_PATH + SHOW_NAME + "/"
-TEST_SHOT_NAME= "01_A"
 
-class TestTemplate(unittest.TestCase):
-
+class TemplateTest(unittest.TestCase):
     def setUp(self):
-        # Create a temporary directory for testing
-        self.temp_dir = TEST_TEMP_DIR_PATH
-        os.mkdir(self.temp_dir)
-
-        self.show_dir=TEST_SHOW_DIR_PATH
-        self.shot_dir=TEST_SHOT_DIR_PATH
-
-        # Create an instance of Show and Shot classes
-        self.show = Show("FilmTitle",1000,"Done",self.show_dir)
-        self.shot = Shot("01_B", 30, "Completed", self.shot_dir)
+        self.show = Show("Mimic", 60, "InProgress", "/Users/moon/Library/CloudStorage/OneDrive-BCIT/TERM 3/P2_DataStructure/TEST/")
+        self.shot = Shot("01_B", 30, "Completed", "/Users/moon/Library/CloudStorage/OneDrive-BCIT/TERM 3/P2_DataStructure/TEST/Show_Shot_DB/Mimic/")
 
     def tearDown(self):
-        # Remove the temporary directory and its contents
-        shutil.rmtree(TEST_TEMP_DIR_PATH+TEST_TEMP_NAME)
+        # Clean up the created directories and files
+        self.show.delete_directory(self.show.path, self.show.name)
+        self.shot.delete_directory(self.shot.path, self.shot.name)
 
     def test_make_directory(self):
-        # Test making a new directory
-        new_dir = os.path.join(self.temp_dir, "NewDirectory")
-        self.show.make_directory(TEST_SHOT_DIR_PATH, TEST_SHOT_NAME)
-        self.assertTrue(os.path.exists(new_dir))
+        # Test creating a directory
+        self.show.make_directory(self.show.path, self.show.name)
+        self.assertTrue(os.path.exists(self.show.path))
 
-        # Test making a directory that already exists
-        self.show.make_directory(self.temp_dir, "NewDirectory")
-        self.assertTrue(os.path.exists(new_dir))
+    def test_create_and_get_all_info(self):
+        # Test creating a show and getting all information
+        self.show.make_directory(self.show.path, self.show.name)
+        self.show.create(self.show.name, self.show.duration, self.show.status, self.show.path, "show_db.json")
+        self.show.get_all_info(self.show.path, "show_db.json")
 
-    def test_delete_directory(self):
-        # Test deleting an existing directory
-        self.show.make_directory(self.temp_dir, "ToDelete")
-        self.show.delete_directory(self.temp_dir, "ToDelete")
-        self.assertFalse(os.path.exists(os.path.join(self.temp_dir, "ToDelete")))
-
-        # Test deleting a non-existing directory
-        non_existing_dir = os.path.join(self.temp_dir, "NonExisting")
-        self.show.delete_directory(self.temp_dir, "NonExisting")
-        self.assertFalse(os.path.exists(non_existing_dir))
-
-    def test_create_and_delete(self):
-        # Test creating a new entry in .json file
-        self.show.create("Mimic", 120, "In Progress", self.temp_dir, "show.json")
-        with open(os.path.join(self.temp_dir, "show.json")) as file:
-            data = load(file)
+        with open(self.show.path + "show_db.json") as file:
+            data = json.load(file)
             self.assertEqual(len(data), 1)
-            self.assertEqual(data[0]["Name"], "Mimic")
+            self.assertEqual(data[0]["Name"], self.show.name)
+            self.assertEqual(data[0]["Time Duration"], self.show.duration)
+            self.assertEqual(data[0]["Status"], self.show.status)
 
-        # Test deleting an entry from .json file
-        self.show.delete("Mimic", self.temp_dir, "show.json")
-        with open(os.path.join(self.temp_dir, "show.json")) as file:
-            data = load(file)
-            self.assertEqual(len(data), 0)
+    def test_get_single_info(self):
+        # Test getting information for a specific show
+        self.show.make_directory(self.show.path, self.show.name)
+        self.show.create(self.show.name, self.show.duration, self.show.status, self.show.path, "show_db.json")
+        self.show.get_single_info(self.show.name, self.show.path, "show_db.json")
+
+        with open(self.show.path + "show_db.json") as file:
+            data = json.load(file)
+            self.assertEqual(len(data), 1)
+            self.assertEqual(data[0]["Name"], self.show.name)
+            self.assertEqual(data[0]["Time Duration"], self.show.duration)
+            self.assertEqual(data[0]["Status"], self.show.status)
 
     def test_edit_name(self):
-        # Test editing the name of an entry in .json file
-        self.show.create("Mimic", 120, "In Progress", self.temp_dir, "show.json")
-        self.show.edit_name(self.temp_dir, "show.json", "Mimic", "New Name")
-        with open(os.path.join(self.temp_dir, "show.json")) as file:
-            data = load(file)
+        # Test editing the name of a show
+        self.show.make_directory(self.show.path, self.show.name)
+        self.show.create(self.show.name, self.show.duration, self.show.status, self.show.path, "show_db.json")
+        self.show.edit_name(self.show.path, "show_db.json", self.show.name, "New Name")
+
+        with open(self.show.path + "show_db.json") as file:
+            data = json.load(file)
+            self.assertEqual(len(data), 1)
             self.assertEqual(data[0]["Name"], "New Name")
 
-    # Add more tests for other methods as needed
+    def test_delete(self):
+        # Test deleting a show
+        self.show.make_directory(self.show.path, self.show.name)
+        self.show.create(self.show.name, self.show.duration, self.show.status, self.show.path, "show_db.json")
+        self.show.delete(self.show.name, self.show.path, "show_db.json")
 
-if __name__ == "__main__":
+        with open(self.show.path + "show_db.json") as file:
+            data = json.load(file)
+            self.assertEqual(len(data), 0)
+
+class ShowTest(unittest.TestCase):
+    def setUp(self):
+        self.show = Show("Mimic", 60, "InProgress", "/Users/moon/Library/CloudStorage/OneDrive-BCIT/TERM 3/P2_DataStructure/TEST/")
+
+    def tearDown(self):
+        self.show.delete_directory(self.show.path, self.show.name)
+
+    def test_make_directory(self):
+        self.show.make_directory(self.show.path, self.show.name)
+        self.assertTrue(os.path.exists(self.show.path))
+
+class ShotTest(unittest.TestCase):
+    def setUp(self):
+        self.shot = Shot("01_B", 30, "Completed", "/Users/moon/Library/CloudStorage/OneDrive-BCIT/TERM 3/P2_DataStructure/TEST/Show_Shot_DB/Mimic/")
+
+    def tearDown(self):
+        self.shot.delete_directory(self.shot.path, self.shot.name)
+
+    def test_make_directory(self):
+        self.shot.make_directory(self.shot.path, self.shot.name)
+        self.assertTrue(os.path.exists(self.shot.path))
+
+if __name__ == '__main__':
     unittest.main()
