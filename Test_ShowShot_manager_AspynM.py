fd79335f58380d461071ce75b3c35c62d90bccdd
aspyn72 <139076588+aspyn72@users.noreply.github.com>
2023-07-15 01:12:47 -0700

    put more description of file_json_name

--- Test_ShowShot_manager_AspynM.py
+++ Test_ShowShot_manager_AspynM.py
@@ -1,94 +1,59 @@
 import unittest
 import os
-import shutil
-from json import load
 import json
 from ShowShot_manager_AspynM import Show, Shot
+import shutil
 
+# Define the unit tests
+class TestShowShotManagement(unittest.TestCase):
 
-class TemplateTest(unittest.TestCase):
     def setUp(self):
-        self.show = Show("Mimic", 60, "InProgress", "/Users/moon/Library/CloudStorage/OneDrive-BCIT/TERM 3/P2_DataStructure/TEST/")
-        self.shot = Shot("01_B", 30, "Completed", "/Users/moon/Library/CloudStorage/OneDrive-BCIT/TERM 3/P2_DataStructure/TEST/Show_Shot_DB/Mimic/")
+        # Create a test directory for the unit tests
+        self.test_dir = "/Users/moon/Desktop/"
+        os.makedirs(self.test_dir, exist_ok=True)
 
     def tearDown(self):
-        # Clean up the created directories and files
-        self.show.delete_directory(self.show.path, self.show.name)
-        self.shot.delete_directory(self.shot.path, self.shot.name)
+        # Delete the test directory and its contents after the unit tests
+        shutil.rmtree(self.test_dir)
 
-    def test_make_directory(self):
-        # Test creating a directory
-        self.show.make_directory(self.show.path, self.show.name)
-        self.assertTrue(os.path.exists(self.show.path))
+    def test_show_creation(self):
+        # Create a show instance and check if the show folder is created
+        show = Show("ShowTitle", 1000, "Done", self.test_dir)
+        show.make_directory(show.path, show.name)
+        self.assertTrue(os.path.exists(show.path + show.name))
 
-    def test_create_and_get_all_info(self):
-        # Test creating a show and getting all information
-        self.show.make_directory(self.show.path, self.show.name)
-        self.show.create(self.show.name, self.show.duration, self.show.status, self.show.path, "show_db.json")
-        self.show.get_all_info(self.show.path, "show_db.json")
+    def test_shot_creation(self):
+        # Create a shot instance and check if the shot folder is created
+        shot = Shot("01_A", 100, "Done", self.test_dir)
+        shot.make_directory(shot.path, shot.name)
+        self.assertTrue(os.path.exists(shot.path + shot.name))
 
-        with open(self.show.path + "show_db.json") as file:
+    def test_show_creation_json(self):
+        # Create a show instance and check if the show JSON file is created
+        show = Show("ShowTitle", 1000, "Done", self.test_dir)
+        show.create("Show1", 1000, "Done", show.path, "show_data.json")
+        self.assertTrue(os.path.exists(show.path + "show_data.json"))
+
+        # Check the content of the show JSON file
+        with open(show.path + "show_data.json") as file:
             data = json.load(file)
             self.assertEqual(len(data), 1)
-            self.assertEqual(data[0]["Name"], self.show.name)
-            self.assertEqual(data[0]["Time Duration"], self.show.duration)
-            self.assertEqual(data[0]["Status"], self.show.status)
+            self.assertEqual(data[0]["Name"], "Show1")
 
-    def test_get_single_info(self):
-        # Test getting information for a specific show
-        self.show.make_directory(self.show.path, self.show.name)
-        self.show.create(self.show.name, self.show.duration, self.show.status, self.show.path, "show_db.json")
-        self.show.get_single_info(self.show.name, self.show.path, "show_db.json")
+    def test_shot_creation_json(self):
+        # Create a shot instance and check if the shot JSON file is created
+        shot = Shot("01_A", 100, "Done", self.test_dir)
+        shot.create("Shot1", 100, "Done", shot.path, "shot_data.json")
+        self.assertTrue(os.path.exists(shot.path + "shot_data.json"))
 
-        with open(self.show.path + "show_db.json") as file:
+        # Check the content of the shot JSON file
+        with open(shot.path + "shot_data.json") as file:
             data = json.load(file)
             self.assertEqual(len(data), 1)
-            self.assertEqual(data[0]["Name"], self.show.name)
-            self.assertEqual(data[0]["Time Duration"], self.show.duration)
-            self.assertEqual(data[0]["Status"], self.show.status)
+            self.assertEqual(data[0]["Name"], "Shot1")
 
-    def test_edit_name(self):
-        # Test editing the name of a show
-        self.show.make_directory(self.show.path, self.show.name)
-        self.show.create(self.show.name, self.show.duration, self.show.status, self.show.path, "show_db.json")
-        self.show.edit_name(self.show.path, "show_db.json", self.show.name, "New Name")
+    # Add more unit tests for other functionalities
 
-        with open(self.show.path + "show_db.json") as file:
-            data = json.load(file)
-            self.assertEqual(len(data), 1)
-            self.assertEqual(data[0]["Name"], "New Name")
-
-    def test_delete(self):
-        # Test deleting a show
-        self.show.make_directory(self.show.path, self.show.name)
-        self.show.create(self.show.name, self.show.duration, self.show.status, self.show.path, "show_db.json")
-        self.show.delete(self.show.name, self.show.path, "show_db.json")
-
-        with open(self.show.path + "show_db.json") as file:
-            data = json.load(file)
-            self.assertEqual(len(data), 0)
-
-class ShowTest(unittest.TestCase):
-    def setUp(self):
-        self.show = Show("Mimic", 60, "InProgress", "/Users/moon/Library/CloudStorage/OneDrive-BCIT/TERM 3/P2_DataStructure/TEST/")
-
-    def tearDown(self):
-        self.show.delete_directory(self.show.path, self.show.name)
-
-    def test_make_directory(self):
-        self.show.make_directory(self.show.path, self.show.name)
-        self.assertTrue(os.path.exists(self.show.path))
-
-class ShotTest(unittest.TestCase):
-    def setUp(self):
-        self.shot = Shot("01_B", 30, "Completed", "/Users/moon/Library/CloudStorage/OneDrive-BCIT/TERM 3/P2_DataStructure/TEST/Show_Shot_DB/Mimic/")
-
-    def tearDown(self):
-        self.shot.delete_directory(self.shot.path, self.shot.name)
-
-    def test_make_directory(self):
-        self.shot.make_directory(self.shot.path, self.shot.name)
-        self.assertTrue(os.path.exists(self.shot.path))
-
+# Run the unit tests
 if __name__ == '__main__':
     unittest.main()
